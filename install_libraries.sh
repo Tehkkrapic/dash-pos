1b29291b21ee4a6b54e3591e109a157c92ed4b5a
Tehkkrapic <35239617+Tehkkrapic@users.noreply.github.com>
2020-03-12 14:24:52 +0100

    Add files via upload
    Script that installs everything needed for ibeacon

---
+++ install_libraries.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install libusb-dev -y
+sudo apt-get install libglib2.0-dev --fix-missing
+sudo apt-get install libudev-dev -y
+sudo apt-get install libical-dev -y
+sudo apt-get install libreadline-dev -y
+sudo apt-get install libdbus-glib-1-dev -y
+sudo apt-get install bluetooth -y
+sudo apt-get install bluez -y
+sudo apt-get install blueman -y
+sudo apt-get install python-bluez -y
+sudo apt-get install python3-tk -y
+sudo apt install bluez-hcidump -y
+pip3 install pillow
+pip3 install pyqrcode
+pip3 install screeninfo
